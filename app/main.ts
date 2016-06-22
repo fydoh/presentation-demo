a5ce88220dca418a9653eb0e3d5b598bb2f234b2
fydoh <david.fye@carvana.com>
2016-06-21 18:35:05 -0700

    cleanup

--- app/main.ts
+++ app/main.ts
@@ -1,9 +1,3 @@
-// declare module 'zone.js/dist/zone' {
-//     export var Zone; // this doesn't actually do anything just makes the compiler not complain about the empty module
-// }
-// declare module 'zone.js/dist/long-stack-trace-zone' {
-//     export var Zone; // this doesn't actually do anything just makes the compiler not complain about the empty module
-// }
 import 'zone.js/dist/zone';
 import 'reflect-metadata';
 
