002a0d520fb22feade6ce872eaaf54e262f396d5
fydoh <david.fye@carvana.com>
2016-06-23 07:27:28 -0700

    universal and preboot

--- app/main.ts
+++ app/main.ts
@@ -1,9 +1,21 @@
-import 'zone.js/dist/zone';
-import 'reflect-metadata';
-import {bootstrap} from '@angular/platform-browser-dynamic';
+// import 'zone.js/dist/zone';
+// import 'reflect-metadata';
+import 'angular2-universal/polyfills';
+
+//import {bootstrap} from '@angular/platform-browser-dynamic';
+
+import { 
+    bootstrap, 
+    enableProdMode,
+    BROWSER_ROUTER_PROVIDERS,
+    BROWSER_HTTP_PROVIDERS,
+} from 'angular2-universal';
+
 import {AppComponent} from './client/app.component';
 
 console.log("bootstrap");
 
-setTimeout(function() {bootstrap(AppComponent).then(function() { alert("done"); }); }, 3000);
-
+setTimeout(function() {bootstrap(AppComponent, [
+  BROWSER_ROUTER_PROVIDERS,
+  BROWSER_HTTP_PROVIDERS
+]).then(function() { alert("done"); }); }, 3000);
--- app/server/server.ts
+++ app/server/server.ts
@@ -1,19 +1,65 @@
+import 'angular2-universal/polyfills';
 
 // Express
 import * as path from 'path';
 import * as express from 'express';
 
+import {
+    provide,
+    expressEngine,
+    ORIGIN_URL,
+    enableProdMode,
+
+    NODE_ROUTER_PROVIDERS,
+    NODE_HTTP_PROVIDERS,
+    REQUEST_URL,
+    BASE_URL
+} from 'angular2-universal';
+
+enableProdMode();
+
+import {AppComponent} from "../client/app.Component";
+
 // Express
 const app = express();
 const ROOT = path.join(path.resolve(__dirname, '../..'));
+
+app.engine('html', expressEngine);
+app.set('view engine', 'html');
 
 // Express
 app.set('views', __dirname);
 
 // Express
-app.get('/', (req, res) => {
-    res.sendFile('index.html', {root: __dirname});
-});
+// app.get('/', (req, res) => {
+//     res.sendFile('index.html', {root: __dirname});
+// });
+
+// Server-side application render (universal)
+function ngApp(req, res) {
+    console.log("here");
+
+    // server side data
+    let baseUrl = '/';
+    let url = req.originalUrl || '/';
+
+    // server side data under providers
+    // also BASE_URL
+    res.render('index', {
+        directives: [AppComponent],
+        platformProviders: [
+            provide(ORIGIN_URL, { useValue: 'http://localhost:3001' }),
+            provide(BASE_URL, { useValue: baseUrl })
+        ],
+        providers: [
+            provide(REQUEST_URL, { useValue: url }),
+            NODE_ROUTER_PROVIDERS,
+            NODE_HTTP_PROVIDERS
+        ],
+        async: false,
+        preboot: true
+    });
+}
 
 // serve static files
 app.use(express.static(ROOT, { index: false }));
@@ -25,7 +71,10 @@ app.get('/cats', (req, res) => {
     })
 });
 
+app.use('/', ngApp);
+
 // server
 app.listen(3001, () => {
     console.log('Listening on http://localhost:3001');
+    //console.log('origin_url', ORIGIN_URL);
 });
