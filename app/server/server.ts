5a4ced208fa8f3dd2b64a6be5d69e00db7a4b772
fydoh <david.fye@carvana.com>
2016-06-26 11:08:11 -0700

    vehicle query

--- app/server/server.ts
+++ app/server/server.ts
@@ -25,6 +25,53 @@ app.get('/cats', (req, res) => {
     })
 });
 
+var vehicles = [
+    {
+        title: '2011 Toyota RAV4',
+        subTitle: 'Limited',
+        mileage: 47413,
+        imageUrl: 'https://az653359.vo.msecnd.net/2000005009/post-large/2000005009-edc-02-search.jpg?v=%20+%202016.6.26_17.59.15',
+        price: 16500,
+        color: 'white',
+        kelleyBlueBookPrice: 18161
+    },
+    {
+        title: '2012 Other Vehicle',
+        subTitle: 'SL1',
+        mileage: 47413,
+        imageUrl: 'https://az653359.vo.msecnd.net/2000005009/post-large/2000005009-edc-02-search.jpg?v=%20+%202016.6.26_17.59.15',
+        price: 16500,
+        color: 'white',
+        kelleyBlueBookPrice: 18161
+    },
+    {
+        title: '2012 Yet Other Vehicle',
+        subTitle: 'SL1',
+        mileage: 47413,
+        imageUrl: 'https://az653359.vo.msecnd.net/2000005009/post-large/2000005009-edc-02-search.jpg?v=%20+%202016.6.26_17.59.15',
+        price: 16500,
+        color: 'black',
+        kelleyBlueBookPrice: 18161
+    }
+];
+
+app.get('/vehicles', (req, res) => {
+    let result = [];
+    console.log(req.query.color);
+
+    if (req.query.color) {
+        vehicles.forEach((vehicle) => {
+            if (vehicle.color === req.query.color) {
+                result.push(vehicle);
+            }            
+        });
+    } else {
+      result = vehicles;  
+    }
+
+    res.json(result);
+});
+
 // server
 app.listen(3001, () => {
     console.log('Listening on http://localhost:3001');
